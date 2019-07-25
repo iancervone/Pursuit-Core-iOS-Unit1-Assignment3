29f89ee9ba760f5b6fb237b6103f5aeade916492
Ian <ian@Ians-MacBook-Pro.local>
2019-07-25 10:25:04 -0400

    add assessment for ian

---
+++ Shopping Cart Pursuit Farms/Shopping Cart Pursuit Farms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Shopping Cart Pursuit Farms
+//
+//  Created by Ian Cervone on 7/25/19.
+//  Copyright © 2019 Ian Cervone. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
